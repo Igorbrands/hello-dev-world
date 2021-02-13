df1c1afc34619cc9df163f8b58c201ec0fb76585
Igor Cantelmo <igorcantelmo@gmail.com>
2021-02-13 13:37:43 -0300

    the script upload

---
+++ my-ubuntu-setup.sh
@@ -0,0 +1,150 @@
+sudo apt update
+
+echo 'installing curl gnupg and apt-transport' 
+sudo apt install apt-transport-https curl gnupg -y
+
+echo 'installing git' 
+sudo apt install git -y
+
+echo "What name do you want to use in GIT user.name?"
+read git_config_user_name
+git config --global user.name "$git_config_user_name"
+clear 
+
+echo "What email do you want to use in GIT user.email?"
+read git_config_user_email
+git config --global user.email $git_config_user_email
+clear
+
+echo "Can I set VIM as your default GIT editor for you? (y/n)"
+read git_core_editor_to_vim
+if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+	git config --global core.editor vim
+else
+	echo "Okay, Let's move on!"
+fi
+
+echo "Generating a SSH Key"     
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+echo 'enabling workspaces for both screens' 
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+echo 'installing zsh'
+sudo apt install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+
+
+export alias pbcopy='xclip -selection clipboard'
+export alias pbpaste='xclip -selection clipboard -o'
+source ~/.zshrc
+
+echo 'installing vim'
+sudo apt install vim -y
+clear
+
+echo 'installing code'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y # or code-insiders
+
+# echo 'installing extensions'
+# code --install-extension dbaeumer.vscode-eslint
+# code --install-extension christian-kohler.path-intellisense
+# code --install-extension dracula-theme.theme-dracula
+# code --install-extension esbenp.prettier-vscode
+# code --install-extension foxundermoon.shell-format
+# code --install-extension pmneo.tsimporter
+# code --install-extension waderyan.gitblame
+# code --install-extension yzhang.markdown-all-in-one
+
+sudo apt update
+sudo apt install snapd -y
+
+echo 'installing spotify' 
+sudo snap install spotify
+
+echo 'installing brave browser'
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+sudo apt install brave-browser
+
+echo 'installing nvm' 
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+nvm --version
+nvm install 12
+nvm alias default 12
+node --version
+npm --version
+
+echo 'installing autosuggestions' 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+echo 'installing theme'
+sudo apt install fonts-firacode -y
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 ~/.oh-my-zsh/themes/node.zsh-theme
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="spaceship"/g' ~/.zshrc
+
+# Opcional, instalar cliente de menssageiros Franz
+# echo 'installing meet franz'
+# wget https://github.com/meetfranz/franz/releases/download/v5.1.0/franz_5.1.0_amd64.deb -O franz.deb
+# sudo dpkg -i franz.debchristian-kohler.path-intellisense
+# sudo apt install -y -f 
+
+echo 'installing slack' 
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
+sudo apt install ./slack-desktop-*.deb -y
+
+echo 'installing docker' 
+sudo apt remove docker docker-engine docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+chmod 777 /var/run/docker.sock
+docker run hello-world
+
+echo 'installing docker-compose' 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo 'installing insomnia'
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+    | sudo apt-key add -
+sudo apt update
+sudo apt install insomnia -y
+
+echo 'installing bkeeper'
+snap install beekeeper-studio
+
+# Cliente de e-mail
+echo 'installing mailspring'
+snap install mailspring
